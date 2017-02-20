25baa002e7f3f95506ae953cafa8c9403ceaa3a9
Crawdaddie52 <dwcrawford52@yahoo.com>
2017-02-20 15:32:54 -0600

    Created Horizontal Scroll at large display and made template for that, created small display change to vertical display but cant fiugre out how to change scrolling back to vertical once at small display. Feel free to add some elements and begin styling, we will keep looking for ways to revert scrolling when at small display

--- js/script.js
+++ js/script.js
@@ -2,16 +2,22 @@
 $(document).ready(function(){
 	// jquery is amde for selecting html elements and performing actions
 	// $('selector').action()
-
-$('p').fadeOut();
-$('.spooky').fadeOut();
-
-$('button').click(function(){
-	$('p').show();
-	$('button').hide();
-})
-$('h2').click(function(){
-	$('h4').slideToggle();
-})
-
+(function() {
+function scrollHorizontally(e) {
+    e = window.event || e;
+    var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+    document.documentElement.scrollLeft -= (delta*30); // Multiplied by 40
+    document.body.scrollLeft -= (delta*30); // Multiplied by 40
+    e.preventDefault();
+}
+if (window.addEventListener) {
+    // IE9, Chrome, Safari, Opera
+    window.addEventListener("mousewheel", scrollHorizontally, false);
+    // Firefox
+    window.addEventListener("DOMMouseScroll", scrollHorizontally, false);
+} else {
+    // IE 6/7/8
+    window.attachEvent("onmousewheel", scrollHorizontally);
+}
+})();
 });
